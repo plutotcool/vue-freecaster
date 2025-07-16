ba8fb401fe990ca501d924730b86905e32fb5f53
juliendargelos <hello@julien.gl>
2025-07-16 11:13:54 +0200

    chore: remove console.log in demo

--- playground/index.ts
+++ playground/index.ts
@@ -3,8 +3,6 @@ import { createApp, defineCustomElement } from 'vue'
 import { FreecasterPlugin } from '../src'
 import App from './App.vue'
 
-console.log(import.meta.env.VITE_CUSTOM_ELEMENT === 'true')
-
 if (import.meta.env.VITE_CUSTOM_ELEMENT === 'true') {
   customElements.define('vue-freecaster', defineCustomElement(App, {
     configureApp(app) {
